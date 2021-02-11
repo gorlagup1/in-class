5a67ebe9868dd0ab3b0baafecf9c460382eb4ccf
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-02-11 08:52:23 -0500

    changes made to singin page

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-   <Home/>  
+   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+   <Home data="Pavani is here" msg="we are friends"/>  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 import Home from './components/Home.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+   // HelloWorld,
     Home
   }
 }
