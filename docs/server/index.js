74d0700ebd89f9e9ccf1e82e3c2b7679029ec56e
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-02-18 13:52:55 -0500

    port!!!

--- docs/server/index.js
+++ docs/server/index.js
@@ -1,17 +1,18 @@
-/* B"H
+/*  B"H
 */
 
 const express = require('express');
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app
-    .get('/',(req, res)=> {
-    res.send('Hello World')
+    .get('/', (req, res) => {
+    res.send('Hello World!')
     })
-    .get('/purim',(req,res) => {
-        res.send('Lechaim!')
+    .get('/purim', (req, res) => {
+        res.send('Lechaim, to life!');
     })
-app.listen(port,() => {
-    console.log('Example app listening at http://localhost:${port}')
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
