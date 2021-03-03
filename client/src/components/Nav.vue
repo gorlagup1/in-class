421ee4e66292fb33c439de004f415196f41c2d3d
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-02 22:21:05 -0500

    vue binding

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,1 +1,73 @@
-h1 class is 
+<template>
+        <nav class="navbar container is-primary" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/">
+            <i class="fab fa-instagram fa-3x  fa-fw fa-pulse"></i>
+            Ista-Gratitude
+          </a>
+      
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+      
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+
+          <router-link to="/" class="navbar-item" >Feed</router-link>
+          <router-link to="/mywall" class="navbar-item" >My Wall</router-link>
+          <router-link to="/Documentation"  class="navbar-item">Documentation</router-link>
+            
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                More
+              </a>
+      
+              <div class="navbar-dropdown">
+                <router-link to="/about" class="navbar-item">
+                  About
+                </router-link>
+
+                <a class="navbar-item" href="jobs.html">
+                  Jobs
+                </a>
+                <a class="navbar-item">
+                  Contact
+                </a>
+                <hr class="navbar-divider">
+                <a class="navbar-item">
+                  Report an issue
+                </a>
+              </div>
+            </div>
+          </div>
+      
+          <div class="navbar-end">
+          <router-link to="/Shop"  class="navbar-item">Shop</router-link>
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a class="button is-light">
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
+
+    
