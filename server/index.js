68151637b42d98225e2123101726934d80d7b758
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-02 23:03:24 -0500

    just static

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 /*  B"H
 */
 
-const express = require('express'); 
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app
+    .use(express.static('./docs'))
     .get('/', (req, res) => {
     res.send('Hello World!')
     })
