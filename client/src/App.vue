2d008381dc6164016a57feee1d478d699bec10dc
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-02 22:59:25 -0500

    App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,72 +1,22 @@
 <template>
   <div id="app">
-        <nav class="navbar container" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="/">
-            <i class="fab fa-instagram fa-3x  fa-fw fa-pulse"></i>
-            Ista-Gratitude
-          </a>
-      
-          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-      
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-
-          <router-link to="/" class="navbar-item" >Home</router-link>
-          <router-link to="/Documentation"  class="navbar-item">Documentation</router-link>
-            
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link">
-                More
-              </a>
-      
-              <div class="navbar-dropdown">
-                <router-link to="/about" class="navbar-item">
-                  About
-                </router-link>
-
-                <a class="navbar-item" href="jobs.html">
-                  Jobs
-                </a>
-                <a class="navbar-item">
-                  Contact
-                </a>
-                <hr class="navbar-divider">
-                <a class="navbar-item">
-                  Report an issue
-                </a>
-              </div>
-            </div>
-          </div>
-      
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="buttons">
-                <a class="button is-primary">
-                  <strong>Sign up</strong>
-                </a>
-                <a class="button is-light">
-                  Log in
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-
+    <MyNav />
     <div class="container">
       <router-view/>
-
     </div>
 
 
   </div>
 </template>
+
+<script>
+import MyNav from "./components/Nav";
+export default {
+  components: {
+    MyNav
+  }
+}
+</script>
 
 <style lang="scss">
 </style>
