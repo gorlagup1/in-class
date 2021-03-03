2ae4baa00bd3aeab7a2be4f26200a581f92e3c39
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-03 15:56:18 -0500

    Home.vue

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" id="home-img">
     <h1> Hi this is new awesome code </h1>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    
   </div>
 </template> 
 
@@ -12,7 +13,7 @@
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
