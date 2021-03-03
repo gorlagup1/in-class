019e3a1672cb3e6442958e09d5500aae195d5ce4
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-02 22:57:24 -0500

    Routing and Composition

---
+++ client/src/views/Documentation.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="notification is-warning is-light">
+        <button class="delete"></button>
+        <h1 class="title is-1">This is the Documentation Page</h1>
+        Primar lorem ipsum dolor sit amet, consectetur
+        adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur.
+    </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
