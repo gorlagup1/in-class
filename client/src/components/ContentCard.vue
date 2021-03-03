4fd5773508014a569c281816b4570bc7d3662f0f
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-02 22:06:30 -0500

    vue binding

---
+++ client/src/components/ContentCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="card">
+      <div class="card-content">
+        <div class="media">
+            <div class="media-left">
+                <figure class="image is-48x48">
+                    <img :src="post.user.pic" :alt="post.user.name" />
+                </figure>
+            </div>
+            <div class="media-content">
+                <p class="title is-4">{{post.user.name}}</p>
+                <p class="subtitle is-6">{{post.user.handle}}</p>
+            </div>
+      </div>
+
+      </div>
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img :src="post.src" :alt="post.alt"
+        />
+      </figure>
+    </div>
+    <div class="card-content">
+
+      <div class="content">
+        {{post.caption}}
+        <br />
+        <time :datetime="post.time">{{post.time}}</time>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        post: Object
+    }
+};
+</script>
+
+<style>
+</style>
+
+    © 2021 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+
+
+   
