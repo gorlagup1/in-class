8a41b122b03fcab240248624c05e63839961b934
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-08 11:27:30 -0500

    Mocked Login

---
+++ client/src/components/LoginBadge.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+
+        <div class="buttons" v-if="!Session.user">
+            <a class="button is-primary">
+                <strong>Sign up</strong>
+            </a>
+            <a class="button is-light" @click="login">
+                Log in
+            </a>
+        </div>
+        <div v-else>
+            Hello Moshe Plotkin
+            (<a @click="logout">
+                Log out
+            </a>)
+        </div>
+    </div>
+</template>
+
+<script>
+import Session from "../models/Session";
+export default {
+    data(){
+        return {
+            Session
+        }
+    },
+    methods: {
+        login(){
+            this.Session.user = { name: "Moshe" }
+        },
+        logout(){
+            this.Session.user = null
+        }
+    }
+}
+</script>
+
+<style>
+</style>
