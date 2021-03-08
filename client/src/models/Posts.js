766638e840b329c8b4091a621d8d77e92933ea14
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-08 15:16:58 -0500

    --- Auto-Git Commit ---

---
+++ client/src/models/Posts.js
@@ -0,0 +1,36 @@
+posts: [
+    { 
+        src: "https://bulma.io/images/placeholders/1280x960.png",
+        alt: "Placeholder image",
+        caption: "Lorem Ipsom",
+        time: Date(),
+        user: {
+            name: "John Smith",
+            handle: "@johnsmith",
+            pic: "https://bulma.io/images/placeholders/96x96.png"
+        }
+    },
+    { 
+        src: "https://bulma.io/images/placeholders/1280x960.png",
+        alt: "Placeholder image",
+        caption: "We want Moshiach Now",
+        time: Date(),
+        user: {
+            name: "Kamala Harris",
+            handle: "@johnsmith",
+            pic: "https://bulma.io/images/placeholders/96x96.png"
+        }
+    },
+    { 
+        src: "https://bulma.io/images/placeholders/1280x960.png",
+        alt: "Placeholder image",
+        caption: "Have a wonderful day",
+        time: Date(),
+        user: {
+            name: "Pavani Gorlagunta",
+            handle: "@johnsmith",
+            pic: "https://bulma.io/images/placeholders/96x96.png"
+        }
+    }, 
+]
+}),
--- client/src/views/MyWall.vue
+++ client/src/views/MyWall.vue
@@ -104,54 +104,30 @@
 
 <script>
 import ContentCard from "../components/ContentCard";
-export default {
+import ContentCreation from "../components/ContentCreation.vue";
+import { } from "../models/Posts";
+
+export default Vue.extend ({
     data: ()=> ({
         newPost: {
             user: { }
         },
-        posts: [
-            { 
-                src: "https://bulma.io/images/placeholders/1280x960.png",
-                alt: "Placeholder image",
-                caption: "Lorem Ipsom",
-                time: Date(),
-                user: {
-                    name: "John Smith",
-                    handle: "@johnsmith",
-                    pic: "https://bulma.io/images/placeholders/96x96.png"
-                }
-            },
-            { 
-                src: "https://bulma.io/images/placeholders/1280x960.png",
-                alt: "Placeholder image",
-                caption: "We want Moshiach Now",
-                time: Date(),
-                user: {
-                    name: "Kamala Harris",
-                    handle: "@johnsmith",
-                    pic: "https://bulma.io/images/placeholders/96x96.png"
-                }
-            },
-            { 
-                src: "https://bulma.io/images/placeholders/1280x960.png",
-                alt: "Placeholder image",
-                caption: "Have a wonderful day",
-                time: Date(),
-                user: {
-                    name: "Pavani Gorlagunta",
-                    handle: "@johnsmith",
-                    pic: "https://bulma.io/images/placeholders/96x96.png"
-                }
-            }, 
-        ]
-    }),
+        posts: []
+    }),  
+    mounted() {
+        this.posts = GetMyPosts(); 
+    },  
     components: {
-        ContentCard
+        ContentCard,
+        ContentCreation
     },
     methods: {
         addPost(){
             this.posts.unshift(this.newPost);
             this.newPost = { user: {} }
+        },
+        deletePost(i){
+            this.posts.
         }
     }
 }
