07d5afb8f1c4368a0c0e8d5885fe99eb5d7b1b1a
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-08 12:20:37 -0500

    hosting the client code on the node server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 /*  B"H
 */
 
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,12 +9,19 @@ const port = process.env.PORT || 3000;
 app
     .use(express.static('./docs'))
     .get('/', (req, res) => {
-    res.send('Hello World!')
+        res.send('Hello World!')
     })
     .get('/purim', (req, res) => {
         res.send('Lechaim, to life!');
+    })
+    // All the way at the end of the pipeline. Return instead of not found
+    .get('*', (req, res) => {
+        res.sendFile( path.join(__dirname, '../docs/index.html' ) );
     })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+  
+
