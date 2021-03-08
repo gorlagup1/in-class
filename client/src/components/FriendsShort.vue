2f553722f890286a8818f0a18650854b2e9e7908
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-08 11:26:39 -0500

    added preview option

---
+++ client/src/components/FriendsShort.vue
@@ -0,0 +1,54 @@
+<template>
+    <article class="panel is-success">
+        <p class="panel-heading">
+            Friends
+        </p>
+        <p class="panel-tabs">
+            <a class="is-active">All</a>
+            <a>Public</a>
+            <a>Private</a>
+            <a>Sources</a>
+            <a>Forks</a>
+        </p>
+        <div class="panel-block">
+            <p class="control has-icons-left">
+            <input class="input is-primary" type="text" placeholder="Search">
+            <span class="icon is-left">
+                <i class="fas fa-search" aria-hidden="true"></i>
+            </span>
+            </p>
+        </div>
+        <a class="panel-block is-active">
+            <span class="panel-icon">
+            <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            bulma
+        </a>
+        <a class="panel-block">
+            <span class="panel-icon">
+            <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            marksheet
+        </a>
+        <a class="panel-block">
+            <span class="panel-icon">
+            <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            minireset.css
+        </a>
+        <a class="panel-block">
+            <span class="panel-icon">
+            <i class="fas fa-book" aria-hidden="true"></i>
+            </span>
+            jgthms.github.io
+        </a>
+        </article>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
