641d3729acf4eed1a3d03782c73eb9cc364b2f49
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-08 11:39:05 -0500

    added preview option

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import FriendShort from"./components/FriendsShort";
 
 import 'bulma';
 import 'buefy/dist/buefy.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './sass/main.scss';
 
-
+Vue.component('FriendsShort', FriendsShort);
 Vue.config.productionTip = false
 
 new Vue({
