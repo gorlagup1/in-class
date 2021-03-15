588b6fbff70e751aaba552c67f3a196d44afe91d
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-15 11:49:41 -0400

    created our first server side controller

--- server/index.js
+++ server/index.js
@@ -4,22 +4,22 @@
 const path = require('path');
 const express = require('express');
 
-const usersCtrl = require('./')
+const usersCtrl = require('./controllers/users');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app
     .use(express.static('./docs'))
-    .get('/', (req, res) => {
-        res.send('Hello World!')
-    })
-    .get('/purim', (req, res) => {
-        res.send('Lechaim, to life!');
-    })
+
+    .use('/users', usersCtrl)
+    
+
     // All the way at the end of the pipeline. Return instead of not found
     .get('*', (req, res) => {
-        res.sendFile( path.join(__dirname, '../docs/index.html' ) );
+        res.sendFile( path.join(__dirname,'../docs/index.html' ));
     })
+    
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
