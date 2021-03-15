a2f0cdb987d232ab31a1ec302e709a37bc202ce9
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-15 12:01:31 -0400

    login and personalization

---
+++ client/src/views/Login.vue
@@ -0,0 +1,44 @@
+<template>
+    <form @submit.prevent="login">
+        <div class="field">
+        <p class="control has-icons-left has-icons-right">
+            <input class="input" type="email" placeholder="Email">
+            <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+            <i class="fas fa-check"></i>
+            </span>
+        </p>
+        </div>
+        <div class="field">
+        <p class="control has-icons-left">
+            <input class="input" type="password" placeholder="Password">
+            <span class="icon is-small is-left">
+            <i class="fas fa-lock"></i>
+            </span>
+        </p>
+        </div>
+        <div class="field">
+        <p class="control">
+            <button class="button is-success">
+            Login
+            </button>
+        </p>
+        </div>
+    </form>
+</template>
+
+<script>
+import { Login } from "../models/Session";
+export default {
+    methods: {
+        login(){
+            Login();
+        }
+    }
+}
+</script>
+
+<style>
+</style>
