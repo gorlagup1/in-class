ef416517aea3b97ac3076d39d0e8da05a2903d22
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-15 11:58:24 -0400

    login and personalization

--- client/src/views/MyWall.vue
+++ client/src/views/MyWall.vue
@@ -9,87 +9,11 @@
           <div class="column">
 
               <div class="content-item">
-                <div class="card">
-                    <div class="card-content">
-                        <form @submit.prevent="addPost">
-                            <div class="field is-horizontal">
-                                <div class="field-label is-normal">
-                                    <label class="label">Owner</label>
-                                </div>
-                                <div class="field-body">
-                                    <div class="field">
-                                    <p class="control is-expanded has-icons-left">
-                                        <input class="input" type="text" placeholder="Name" v-model="newPost.user.name">
-                                        <span class="icon is-small is-left">
-                                        <i class="fas fa-user"></i>
-                                        </span>
-                                    </p>
-                                    </div>
-                                    <div class="field">
-                                    <p class="control is-expanded has-icons-left has-icons-right">
-                                        <input class="input" type="text" placeholder="Socialmedia Handle" v-model="newPost.user.handle" >
-                                        <span class="icon is-small is-left">
-                                        <i class="fas fa-envelope"></i>
-                                        </span>
-                                        <span class="icon is-small is-right">
-                                        <i class="fas fa-check"></i>
-                                        </span>
-                                    </p>
-                                    </div>
-                                </div>
-                                </div>
-
-                                <div class="field is-horizontal">
-                                <div class="field-label is-normal">
-                                    <label class="label">Picture URL</label>
-                                </div>
-                                <div class="field-body">
-                                    <div class="field">
-                                    <div class="control">
-                                        <input class="input" type="text" placeholder="http://google.com" v-model="newPost.src">
-                                    </div>
-                                    <p class="help is-danger">
-                                        This field is required
-                                    </p>
-                                    </div>
-                                </div>
-                                </div>
-
-                                <div class="field is-horizontal">
-                                <div class="field-label is-normal">
-                                    <label class="label">Caption</label>
-                                </div>
-                                <div class="field-body">
-                                    <div class="field">
-                                    <div class="control">
-                                        <textarea class="textarea" placeholder="Tell us what you are thinking" v-model="newPost.caption"></textarea>
-                                    </div>
-                                    </div>
-                                </div>
-                                </div>
-
-                                <div class="field is-horizontal">
-                                <div class="field-label">
-                                    <!-- Left empty for spacing -->
-                                </div>
-                                <div class="field-body">
-                                    <div class="field">
-                                    <div class="control">
-                                        <button class="button is-primary">
-                                        Post
-                                        </button>
-                                    </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </form>
-                    </div>
-                </div>
+                  <content-creation :newPost="newPost" @add="addPost" />
               </div>
 
               <div class="content-item" v-for="(post, i) in posts" :key="i">
-                <ContentCard :post="post" />
+                <ContentCard :post="post" @delete="deletePost(i)" />
               </div>
           </div>
           <div class="column is-one-quarter">
@@ -103,20 +27,20 @@
 </template>
 
 <script>
+import Vue from "vue";
 import ContentCard from "../components/ContentCard";
-import ContentCreation from "../components/ContentCreation.vue";
-import { } from "../models/Posts";
-
-export default Vue.extend ({
+import ContentCreation from '../components/ContentCreation.vue';
+import { GetMyPosts } from "../models/Posts";
+export default Vue.extend({
     data: ()=> ({
         newPost: {
             user: { }
         },
         posts: []
-    }),  
+    }),
     mounted() {
-        this.posts = GetMyPosts(); 
-    },  
+        this.posts = GetMyPosts();
+    },
     components: {
         ContentCard,
         ContentCreation
@@ -127,10 +51,10 @@ export default Vue.extend ({
             this.newPost = { user: {} }
         },
         deletePost(i){
-            this.posts.
+            this.posts.splice(i, 1);
         }
     }
-}
+})
 </script>
 
 <style>
