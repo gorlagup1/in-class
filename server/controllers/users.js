18aed935aa5e0f357f986efac816cc6d0e0066a5
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-18 14:39:38 -0400

    changes made in server

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,34 +1,37 @@
 /* B"H
-
 */
 const express = require('express');
 const model = require('../models/users');
 
-const app =express.Router();
+const app = express.Router();
 
     app
-        .get('/', (req, res)=> {
-            res.send(model.GetAll() ) ;
+        .get('/', (req, res)=>{
+            res.send( model.GetAll() );
+            console.log(req.headers);            
+        }  )
+        .get('/:user_id',(req, res)=> res.send( model.Get(req.params.user_id) ))
+        .post('/', (req, res)=> { 
+            res.send( model.Add({
+                firstName: req.body.firstName,
+                lastName:  req.body.lastName,
+                handle: req.body.handle,
+                pic:  req.body.pic,
+            }) );
             console.log(req.headers);
         })
-        .get('/:user_id',(req, res)=> res.send(model.Get(req.params.user_id))
-        .post('/',(req,res)=>{
-            res.send(model.Add({
-            firstName: req.query.firstName,
-            lastName: req.query.lastName,
-            handle: req.query.handle,
-            pic: req.query.pic,
-        }));
-        console.log(req.headers);
-    })
-        .patch('/:user_id',(req,res)=>res.send( model.Update(
+        .patch('/:user_id', (req, res)=> res.send( model.Update(
             req.params.user_id,
             {
-                firstName: req.query.firstName,
-                lastName: req.query.lastName,
-                handle: req.query.handle,
-                pic: req.query.pic,  
-            })))
-        .delete('/:user_id',(res,req)=> res.send(model.Delete(req.parmas.user_id)))
+                firstName: req.body.firstName,
+                lastName:  req.body.lastName,
+                handle: req.body.handle,
+                pic:  req.body.pic,
+        }) ) )
+        .delete('/:user_id', (req, res)=> res.send( model.Delete(req.params.user_id) ) )
 
-        module.exports = app;
+
+module.exports = app;
+
+    
+
--- server/models/users.js
+++ server/models/users.js
@@ -2,26 +2,61 @@
 */
 
 const list = [
-    { name: 'Pavani Gorlagunta'}
+    { 
+        firstName: 'Pavani',
+        lastName: 'Gorlagunta',
+        handle: '@JewPaltz',
+        pic: 'https://bulma.io/images/placeholders/96x96.png',
+        password: 'Me',
+    },
+    { 
+        firstName: 'Kamala',
+        lastName: 'Harris',
+        handle: '@vpotus',
+        pic: 'https://bulma.io/images/placeholders/96x96.png',
+        password: 'Her',
+    },
+    { 
+        firstName: 'John',
+        lastName: 'Smith',
+        handle: '@johnsmith',
+        pic: 'https://bulma.io/images/placeholders/96x96.png',
+        password: 'BeepBop',
+    },
+
 ];
 
 module.exports.GetAll = ()=> list;
 module.exports.Get = (user_id)=> list[user_id];
 module.exports.Add = (user)=> {
-    list.push(user);
-    return user;
+    if(!user.firstName){
+        throw "First Name is reqired"
+    }
+     list.push(user);
+     return { ...user, password: undefined };
 }
-module.exports.Update = (user_id,user)=> {
+module.exports.Update = (user_id, user)=> {
     const oldObj = list[user_id];
-    
-    console.log({oldobj, user});
-    const newObj = { ...oldObj, ...user};
-    console.log({ newObj})
-    list[user_id] = { ...list[user_id], ...user};
-    return user;
+    if(user.firstName){
+        oldObj.firstName = user.firstName;
+    }
+    if(user.lastName){
+        oldObj.lastName = user.lastName;
+    }
+    if(user.handle){
+        oldObj.handle = user.handle;
+    }
+    if(user.pic){
+        oldObj.pic = user.pic;
+    }
+    //list[user_id] = newObj ;
+    return { ...oldObj, password: undefined };
 }
 module.exports.Delete = (user_id)=> {
-   const user = list[user_id];
-    list.splice(user_id,1);
+    const user = list[user_id];
+    list.splice(user_id, 1);
     return user;
 }
+
+    
+
