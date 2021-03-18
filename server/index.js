1cd6ee9e74da8dc1f0168c39d9958434968192a2
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-18 14:46:05 -0400

    index.js updated

--- server/index.js
+++ server/index.js
@@ -10,21 +10,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app
-.use(express.json)
-.use(express.static('./docs'))
+    .use(express.json())
+    .use(express.static('./docs'))
 
     .use('/users', usersCtrl)
-    
+
 
     // All the way at the end of the pipeline. Return instead of not found
     .get('*', (req, res) => {
-        res.sendFile( path.join(__dirname,'../docs/index.html' ));
+        res.sendFile( path.join(__dirname, '../docs/index.html' ) );
     })
-    
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-  
-
+   
