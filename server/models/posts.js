5aca90a043d5fdc1713b8d697258508791fd76d9
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-29 13:02:28 -0400

    error handling

--- server/models/posts.js
+++ server/models/posts.js
@@ -2,7 +2,7 @@
 */
 const users = require("./users");
 
-const list = [
+const list = [ 
     { 
         src: "https://bulma.io/images/placeholders/1280x960.png",
         alt: "Placeholder image",
