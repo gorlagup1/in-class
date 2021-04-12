a65a9325b8514728105167e123d1ae3e96dae73c
gorlagup1 <gorlagup1@hawkmail.newpaltz.edu>
2021-04-12 09:06:05 -0400

    client and server are communicating

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" id="home-img">
-    <h1> Hi this is new awesome code </h1>
-
+    <h1> Hi this is new awesome code</h1>
+    
     
   </div>
-</template> 
+</template>
 
 <script>
 // @ is an alias to /src
-
+import { api } from "../models/myFetch";
+(async ()=>{
+  const actualItem = await api("posts");
+  console.log({actualItem});
+})()
 export default {
   name: 'Home',
   components: {
     
   }
 }
-</script> 
+</script>
+
+    
