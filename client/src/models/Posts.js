394a106f0a56ffe0ba742c9bfbb4a550b0026384
gorlagup1 <gorlagup1@hawkmail.newpaltz.edu>
2021-04-16 21:39:38 -0400

    Login is working!!!

--- client/src/models/Posts.js
+++ client/src/models/Posts.js
@@ -1,52 +1,19 @@
-import Session from "./Session";
+//import Session from "./Session";
+import { api } from "../models/myFetch";
+
 
 /* B"H
     Access to all the posts
 */
-const posts = [
-    { 
-        src: "https://bulma.io/images/placeholders/1280x960.png",
-        alt: "Placeholder image",
-        caption: "Lorem Ipsom",
-        time: Date(),
-        user: {
-            name: "John Smith",
-            handle: "@johnsmith",
-            pic: "https://bulma.io/images/placeholders/96x96.png"
-        }
-    },
-    { 
-        src: "https://bulma.io/images/placeholders/1280x960.png",
-        alt: "Placeholder image",
-        caption: "We want Moshiach Now",
-        time: Date(),
-        user: {
-            name: "Kamala Harris",
-            handle: "@johnsmith",
-            pic: "https://bulma.io/images/placeholders/96x96.png"
-        }
-    },
-    { 
-        src: "https://bulma.io/images/placeholders/1280x960.png",
-        alt: "Placeholder image",
-        caption: "Have a wonderful day",
-        time: Date(),
-        user: {
-            name: "Pavani Gorlagunta",
-            handle: "@gorlagup1",
-            pic: "https://bulma.io/images/placeholders/96x96.png"
-        }
-    },
-];
 
 export function GetMyPosts(){
-    return GetPostsForUser(Session.user.handle);
+    return api("posts")
 }
 
-export function GetPostsForUser(id){
-    return posts.filter( x=> x.user.handle == id );
+export function GetPostsForUser(handle){
+    return api(`posts/${handle}`)
 }
 
 export function GetMyFeed(){
-    return posts;
+    return api("posts/feed");
 }
--- client/src/models/Session.js
+++ client/src/models/Session.js
@@ -1,16 +1,35 @@
 /* B"H
     Holds the user session. Is a singleton.
 */
+import { ToastProgrammatic as toastr } from "buefy";
+import { api } from "./myFetch";
+
 const Session = {
     user: null,
     message: [] // To be used later
 }
 export default Session;
 
-export function Login(){
-    Session.user = { name: "Pavani", handle: '@gorlagup1' }
+export async function Login(handle, password){
+    const {user, token} = await api('users/login', { handle, password })
+    Session.user = user;
+    Session.token = token;
+
+    console.log(Session.user);
+    toastr.open({
+        type: 'is-success',
+        message: `Welcome ${Session.user.firstName}`
+    })
 }
 
 export function Logout(){
     Session.user = null
+}
+
+export function toastError(msg){
+    toastr.open({
+        message: msg,
+        queue: false,
+        type: 'is-danger'
+    })
 }
