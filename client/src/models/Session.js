1a89d6641854d1efcfefffd8c6c857b4508c0a66
gorlagup1 <gorlagup1@hawkmail.newpaltz.edu>
2021-04-19 21:41:42 -0400

    token is being passed with every Request

--- client/src/models/Session.js
+++ client/src/models/Session.js
@@ -2,10 +2,13 @@
     Holds the user session. Is a singleton.
 */
 import { ToastProgrammatic as toastr } from "buefy";
+import router from "../router";
 import { api } from "./myFetch";
 
 const Session = {
     user: null,
+    token:null,
+    nextRoute:null,
     message: [] // To be used later
 }
 export default Session;
@@ -19,7 +22,8 @@ export async function Login(handle, password){
     toastr.open({
         type: 'is-success',
         message: `Welcome ${Session.user.firstName}`
-    })
+    });
+    router.push(Session.nextRoute ?? '/feed')
 }
 
 export function Logout(){
