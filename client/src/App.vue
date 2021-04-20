a3fbac8bdd8ed9bbfb106083438e439e2c4b960f
gorlagup1 <gorlagup1@hawkmail.newpaltz.edu>
2021-04-19 23:08:01 -0400

    Routing and Composition

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <MyNav />
+    <div class="container">
+      <router-view/>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+import MyNav from "./components/Nav";
+export default {
+  components: {
+    MyNav
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
