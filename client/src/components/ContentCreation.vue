ceaf52c0db50c1ce52f861c529da041ec4141f78
gorlagup1 <gorlagup1@hawkmail.newpaltz.edu>
2021-04-19 21:30:32 -0400

    display owner info properly

--- client/src/components/ContentCreation.vue
+++ client/src/components/ContentCreation.vue
@@ -2,48 +2,24 @@
                   <div class="card">
                     <div class="card-content">
                         <form @submit.prevent="$emit('add')">
-                            <div class="field is-horizontal">
-                                <div class="field-label is-normal">
-                                    <label class="label">Owner</label>
-                                </div>
-                                <div class="field-body">
-                                    <div class="field">
-                                    <p class="control is-expanded has-icons-left">
-                                        <input class="input" type="text" placeholder="Name" v-model="newPost.user.name">
-                                        <span class="icon is-small is-left">
-                                        <i class="fas fa-user"></i>
-                                        </span>
-                                    </p>
-                                    </div>
-                                    <div class="field">
-                                    <p class="control is-expanded has-icons-left has-icons-right">
-                                        <input class="input" type="text" placeholder="Socialmedia Handle" v-model="newPost.user.handle" >
-                                        <span class="icon is-small is-left">
-                                        <i class="fas fa-envelope"></i>
-                                        </span>
-                                        <span class="icon is-small is-right">
-                                        <i class="fas fa-check"></i>
-                                        </span>
-                                    </p>
-                                    </div>
-                                </div>
-                                </div>
 
-                                <div class="field is-horizontal">
+                            <div class="field is-horizontal">
                                 <div class="field-label is-normal">
                                     <label class="label">Picture URL</label>
                                 </div>
                                 <div class="field-body">
                                     <div class="field">
                                     <div class="control">
-                                        <input class="input" type="text" placeholder="http://google.com" v-model="newPost.src">
-                                    </div>
-                                    <p class="help is-danger">
-                                        This field is required
-                                    </p>
+                                    <input class="input" type="text" placeholder="http://google.com" v-model="newPost.src">
+                                       </div>
+                                       <p class="help is-danger">
+                                           This field is required
+                                       </p>
                                     </div>
                                 </div>
                                 </div>
+                                
+                  
 
                                 <div class="field is-horizontal">
                                 <div class="field-label is-normal">
