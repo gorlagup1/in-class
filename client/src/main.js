aecd8473320639ae708921471459378621132958
gorlagup1 <gorlagup1@hawkmail.newpaltz.edu>
2021-04-19 23:06:16 -0400

    added buefy javascript

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import Buefy from "buefy";
+
 import App from './App.vue'
 import router from './router'
 import FriendsShort from "./components/FriendsShort";
@@ -8,6 +10,7 @@ import 'buefy/dist/buefy.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './sass/main.scss';
 
+Vue.use(Buefy);
 Vue.component('FriendsShort', FriendsShort);
 Vue.config.productionTip = false
 
