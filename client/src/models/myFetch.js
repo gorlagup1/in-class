66c5e3d494c0bf48a8694db727fde11c95846574
gorlagup1 <gorlagup1@hawkmail.newpaltz.edu>
2021-04-19 21:42:42 -0400

    Deleting posts works

--- client/src/models/myFetch.js
+++ client/src/models/myFetch.js
@@ -1,10 +1,37 @@
 /* B"H
     A wrapper for the native fetch api
 */
+import Session, { toastError } from "./Session";
+
 export const API_ROOT = process.env.VUE_APP_API_ROOT;
 
-export function api(url){
+export function api(url, data, method){
 
-    return fetch(API_ROOT + url).then(x=> x.json());
+    let promise;
 
+    const headers = { authorization: `bearer ${Session.token}` };
+
+    if(data){
+        promise = fetch(API_ROOT + url, {
+            method: method ?? 'POST', // *GET, POST, PUT, DELETE, etc.
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+          });
+    }else{
+        promise = fetch(API_ROOT + url, { headers });
+
+    }
+    return promise
+        .then(x=> {
+            if(x.ok) return x.json();
+            else return x.json().then(y=> { throw y; })
+        })
+        .catch(err=>{
+            console.error(err);
+            toastError(err.msg || err);
+        });
 }
