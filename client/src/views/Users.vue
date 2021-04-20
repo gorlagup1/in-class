8d9fdc1ce6fded6640392228982cbfc28b7d293e
gorlagup1 <gorlagup1@hawkmail.newpaltz.edu>
2021-04-19 23:38:11 -0400

    added admin page to display users

--- client/src/views/Users.vue
+++ client/src/views/Users.vue
@@ -1,22 +1,43 @@
 <template>
-  <div id="app">
-    <MyNav />
-    <div class="container">
-      <router-view/>
-    </div>
+  <div id="users">
+      <div class="columns">
+          <div class="column">
+              <h1 class="title is-1">Users Page</h1>
+                <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+                    <tr><th>First Name</th><th>Last Name</th><th>Handle</th></tr>
 
+                    <tr v-for="user in users" :key="user.handle">
+                        <td>{{user.firstName}}</td>
+                        <td>{{user.lastName}}</td>
+                        <td>{{user.handle}}</td>
+                    </tr>
 
+                </table>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-import MyNav from "./components/Nav";
-export default {
-  components: {
-    MyNav
-  }
-}
+import Vue from "vue";
+import { GetAllUsers } from "../models/Users";
+export default Vue.extend({
+    data: ()=> ({
+        users: []
+    }),
+    async mounted() {
+        this.users = await GetAllUsers();
+    },
+    components: {
+    },
+    methods: {
+    }
+})
 </script>
 
-<style lang="scss">
+<style>
+.content-item {
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
 </style>
