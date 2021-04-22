a4a711615ec93831450e5b4da15a03d8086d186f
gorlagup1 <gorlagup1@hawkmail.newpaltz.edu>
2021-04-22 14:56:43 -0400

    added facebook page

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,18 +5,41 @@
       <router-view/>
     </div>
 
+    <div id = "fb-root"></div>
+    <div>
 
-  </div>
+
+ </div>
 </template>
+
 
 <script>
 import MyNav from "./components/Nav";
+
 export default {
   components: {
-    MyNav
+    MyNav  
   }
 }
+        /*global fb*/
+        window.fbAsyncInit = function(){
+          FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+          });
+        };
+
+        (function(d, s, id){
+          var js, fjs = d.getElementByTagName(s)[0];
+          if(d.getElementByiD(ID)) RETURN;
+          JS = D.CREATEeLEMENT(S); JS.ID = ID;
+          JS.SRC = 'https://connect.facebook.net/en_us/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+        
 </script>
 
 <style lang="scss">
+
 </style>
