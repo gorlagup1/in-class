2c2a70c017b68c4150176f32bb8dc09b5ceb664b
gorlagup1 <gorlagup1@hawkmail.newpaltz.edu>
2021-04-28 15:40:54 -0400

    Accessing FB from server

--- client/src/models/Session.js
+++ client/src/models/Session.js
@@ -26,6 +26,21 @@ export async function LoginFB(access_token){
     router.push(Session.nextRoute ?? '/feed')
 }
 
+export async function LoginFB(access_token){
+   const {user, token} = await api('users/loginFB', { access_token })
+   Session.user = user;
+   Session.token = token;
+
+   console.log(Session.user);
+   toastr.open({
+       type: 'is-success',
+       message: 'Welcome ${Session.user.firstName}'
+   });
+
+   router.push(Session.nextRoute ?? 'feed')
+
+}
+
 export function Logout(){
     Session.user = null
 }
