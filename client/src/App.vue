3e864bfbb62847af5d9b7239eb000ba316533d76
gorlagup1 <gorlagup1@hawkmail.newpaltz.edu>
2021-04-28 16:19:03 -0400

    Accessing FB from server

--- client/src/App.vue
+++ client/src/App.vue
@@ -37,7 +37,7 @@ export default {
 
         (function(d, s, id){
           var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) {return;}
+          if (d.getElementById(id)) return;
           js = d.createElement(s); js.id = id;
           js.src = 'https://connect.facebook.net/en_US/sdk.js';
           fjs.parentNode.insertBefore(js, fjs);
