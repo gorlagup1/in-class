384ad97b286a55fd077766e77b6596a1d1dcd2e2
gorlagup1 <gorlagup1@hawkmail.newpaltz.edu>
2021-04-28 16:19:30 -0400

    Accessing FB from server

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -27,6 +27,13 @@ const app = express.Router();
             .catch(next);
 
         })
+        .post('/loginFB', (req, res, next)=> {
+
+            model.LoginFB(req.body.access_token)
+            .then(user=> res.send( user ))
+            .cattch(next);
+            
+        })
         .patch('/:user_id', LoginRequired, (req, res)=> res.send( model.Update( req.params.user_id, req.body ) ) )
         .delete('/:user_id', LoginRequired, (req, res)=> res.send( model.Delete(req.params.user_id) ) )
 
